8fbb27b28185ee7b7b79fa7f004386cca0dac4f8
Hamilton Saxion <hsaxion8@gmail.com>
2019-03-01 11:32:08 -0600

    added files

---
+++ app/data/friends.js
@@ -0,0 +1,18 @@
+var friendsArray = [{
+	"name": "Guss",
+	"photo": "https://i.pinimg.com/originals/61/02/ca/6102cae0cdc9933c678b4a4b8ab10b17.jpg",
+	"scores": [
+			5,
+			1,
+			4,
+			4,
+			5,
+			1,
+			2,
+			5,
+			4,
+			1
+	]
+}];
+
+module.exports = friendsArray;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,43 @@
+
+var friendsData = require("../data/friends.js");
+
+module.exports = function(app) {
+
+    app.get("/api/friends", function(req, res) {
+
+        res.json(friendsData);
+    });
+
+    function findBestFriend(user) {
+        var userScores = user.scores;
+        var maxDiff = 4 * 10;
+        var winningFriend;
+  
+        for (var i = 0; i < friendsData.length; i++) {
+            var friendScores = friendsData[i].scores;
+            var totalDiff = 0;
+            // // iterate thru friend's scores
+            for (var j = 0; j < friendScores.length; j++) {
+                totalDiff = totalDiff + Math.abs(userScores[j] - friendScores[j]);
+            }
+            if (totalDiff <= maxDiff) {
+                winningFriend = friendsData[i];
+                maxDiff = totalDiff;
+            }
+        }
+        return winningFriend;
+    }
+
+    app.post("/api/friends", function(req, res) {
+        var currentUser = req.body;
+        var bestFriend = findBestFriend(currentUser);
+        res.json(bestFriend);
+        friendsData.push(currentUser);
+    });
+    app.post("/api/clear", function() {
+        // Empty out the arrays of data
+        friendsData = [];
+
+        console.log(friendsData);
+    });
+};
